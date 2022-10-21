a40a1c0d252293155c34976efc9872ac38fb9888
maruf <dev.maruf112@gmail.com>
2022-10-21 09:57:19 +0600

    successful cl

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const port = 5000;
 const cors = require("cors");
 app.use(cors());
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 app.get("/", (req, res) => {
   res.send("News api is running.");
 });
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+// app.get("/news", (req, res) => {
+//   res.send(news);
+// });
+app.get("/news/:id", (req, res) => {
+  console.log(req.params.id);
 });
 app.listen(port, () => {
   console.log("Example listening on the port", port);
